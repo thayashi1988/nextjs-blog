fbcd13ce65e6b87d50f6b76b31c438700d6ea98f
thayashi1988 <tatsurouhayashi0210@gmail.com>
2021-01-05 00:23:37 +0900

    見出し変更

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ca71c77fa936f2609870";
+/******/ 	var hotCurrentHash = "2111605f5f05757ddc19";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,8 @@ export async function getStaticProps() {
   }
 }
 
+const name = 'じぃのNext.jsアプリ'
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -21,7 +23,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>{ name }</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
