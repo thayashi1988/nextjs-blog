bd084627ece0a3c7022c8470002f025f25021cb7
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-04-17 16:54:09 +0900

    ♻️ ドラッグ対応のため調整中

--- src/components/Hunter/HunterList.tsx
+++ src/components/Hunter/HunterList.tsx
@@ -3,19 +3,37 @@ import type { DOMAttributes, VFC } from 'react';
 import React from 'react';
 
 type PROPS = {
+  innerRef?: string;
   style?: object;
   class?: string;
   innerHTML?: any;
   children?: React.ReactNode;
 };
 
-export const HunterList: VFC<PROPS> = (props) => {
+export const HunterList = React.forwardRef((props: any, ref: any) => {
+  const provided = props.provided;
+
   return (
     <div
+      ref={ref}
+      {...provided.draggableProps}
+      {...provided.dragHandleProps}
       style={props.style}
-      className={props.class}
+      className={`w-full text-white text-xs border border-solid border-blue-600 flex items-start justify-center bg-gray-200 bg-opacity-60 ${props.class}`}
       dangerouslySetInnerHTML={props.innerHTML}>
       {props.children}
     </div>
   );
-};
+});
+
+// export const HunterList: VFC<PROPS> = (props) => {
+//   return (
+//     <div
+//       ref={props.innerRef}
+//       style={props.style}
+//       className={`w-full text-white text-xs border border-solid border-blue-600 flex items-start justify-center bg-gray-200 bg-opacity-60 ${props.class}`}
+//       dangerouslySetInnerHTML={props.innerHTML}>
+//       {props.children}
+//     </div>
+//   );
+// };
--- src/pages/hunter/index.tsx
+++ src/pages/hunter/index.tsx
@@ -1,15 +1,24 @@
 import React, { useState, useRef } from 'react';
 import type { NextPage } from 'next';
+import { renderToString } from 'react-dom/server';
+
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  resetServerContext,
+} from 'react-beautiful-dnd';
 
 import Head from 'next/head';
 import Layout from 'src/components/Layout/layout';
-import { NextImg } from 'src/components/Img/Img';
 import { TextInput } from 'src/components/Hunter/HunterTextInput';
 import { HunterList } from 'src/components/Hunter/HunterList';
 import { HunterListWrap } from 'src/components/Hunter/HunterListWrap';
 import { HunterWidthResult } from 'src/components/Hunter/HunterWidthResult';
 import { HunterBothResult } from 'src/components/Hunter/HunterBothResult';
 import { HunterEvenRows } from 'src/components/Hunter/HunterEvenRows';
+import { HunterHunter } from 'src/components/Hunter/HunterHunter';
+import { HunterDiv } from 'src/components/Hunter/HunterDiv';
 
 // interface VARIABLE{
 //   widthInputText: number;
@@ -19,23 +28,13 @@ import { HunterEvenRows } from 'src/components/Hunter/HunterEvenRows';
 //   divisionRemainderHeightValue: string | number;
 // };
 
+type HunterObj = {
+  divisionJustText: string | number;
+  divisionRemainderText: string | number;
+  isTextHidden: boolean;
+};
+
 export const Index: NextPage = () => {
-  const pra: any = useRef(null);
-  // console.log(pra.current.innerHTML);
-
-  // const onmousedown = (event) => {
-  //   document.addEventListener('mousemove', onMouseMove);
-  // };
-  // var onMouseMove = (event) => {
-  //   var x = event.clientX;
-  //   var y = event.clientY;
-  //   var width = pra.current.offsetWidth;
-  //   var height = pra.current.offsetHeight;
-  //   pra.current.style.position = 'absolute';
-  //   pra.current.style.top = y - height / 2 + 'px';
-  //   pra.current.style.left = x - width / 2 + 'px';
-  // };
-
   //////// useState  ////////
   // 幅と高さを入力するテキストボックス
   const [widthInputText, setWidthInputText] = useState<string>('');
@@ -59,14 +58,17 @@ export const Index: NextPage = () => {
 
   let hunterListWrapBoxWidth: any = '25'; // HunterListの親ボックスの幅
   const divisionRemainderWidth: number = parseFloat(widthInputText) % 303; // 幅にあまりがあるか
-  let divisionRemainderWidthValue: number = divisionRemainderWidth; // 割ったあまりの数字
-  let divisionRemainderBoxWidthPosition: number = 0;
+  const divisionRemainderWidthValue: number = divisionRemainderWidth; // 割ったあまりの数字
+  let divisionRemainderBoxWidthPosition: number = 0; // 幅が余る場合、横にずらすための数字を格納
+  let divisionRemainderText: string = ''; //余りの場合の数値を格納
 
-  let divisionRemaindertext: string = '';
-  let divisionRemainderHeightValueText: string | number = '';
-  let simulationFlag: boolean = false;
-  const loopCount: number[] = [1, 2, 3, 4];
+  let divisionRemainderHeightValueText: string | number = ''; // 余りのテキストを格納
+  let simulationFlag: boolean = false; // 計算ボックスを表示させるフラグ
+  const arrayObj: HunterObj[] = []; //ボックス生成のためのオブジェクト
 
+  const refRef = useRef();
+
+  // 幅、高さが両方入力されたらボックス生成開始
   if (widthInputText !== '' && heightInputText !== '') {
     simulationFlag = true;
   }
@@ -88,63 +90,25 @@ export const Index: NextPage = () => {
     divisionRemainderHeightValue = divisionRemainderHeight;
   }
 
-  // HunterListボックス生成
-  const ShowHunterList = (props) => {
-    const isShow: Boolean = props.isShow;
-    const isTextHidden: Boolean = props.isTextHidden;
-    if (isShow) {
-      // (parseFloat(heightInputText) / 1820)で割れた数だけhunterlistボックス生成を生成
-      const hunterListCreateElem: any = [];
-      for (let i = 0; i < grayBoxNum; i++) {
-        hunterListCreateElem.push(i);
-      }
+  // mapのためにオブジェクト生成
+  const hunterListCreateElem: any = [];
+  for (let i = 0; i < grayBoxNum; i++) {
+    hunterListCreateElem.push({ id: Math.floor(Math.random() * 10000001) * i });
+  }
+  // console.log('grayBoxNum:', grayBoxNum);
 
-      // 高さの余りが910で割り切れれば、1820の倍数なのでピッタリ
-      if (divisionRemainderHeightValue % 910 === 0) {
-        divisionJustText = '1820mmピッタリ';
-        divisionRemaindertext = `<span class="block text-center text-black w-full" style="height: ${divisionResultStyles.height}">${divisionJustText}</span>`;
-      } else {
-        //割り切れなければ、余りを表示と「1820 - 余り」を表示
-        divisionRemaindertext = `<span class="block text-center text-black text-xs w-full" style="height: ${
-          divisionResultStyles.height
-        }%">埋められるのは${divisionRemainderHeightValue}mm</span>
+  // 高さの余りが910で割り切れれば、1820の倍数なのでピッタリ
+  if (divisionRemainderHeightValue % 910 === 0) {
+    divisionJustText = '1820mmピッタリ';
+    divisionRemainderText = `<span class="block text-center text-black w-full" style="height: ${divisionResultStyles.height}">${divisionJustText}</span>`;
+  } else {
+    //割り切れなければ、余りを表示と「1820 - 余り」を表示
+    divisionRemainderText = `<span class="block text-center text-black text-xs w-full" style="height: ${
+      divisionResultStyles.height
+    }%">埋められるのは${divisionRemainderHeightValue}mm</span>
         <span class="block text-center text-black text-xs w-full"
         }%">余るのは${1820 - divisionRemainderHeightValue}mm</span>`;
-      }
-
-      return hunterListCreateElem.map((i: React.Key) => {
-        // 偶数列は余りのテキストを出さない処理
-        if (isTextHidden) {
-          return (
-            <HunterList
-              key={i}
-              class="w-full flex items-start justify-center text-white text-xs border border-solid border-blue-600 bg-gray-200 bg-opacity-60"
-              style={hunterListStyles}></HunterList>
-          );
-        }
-
-        // HunterListの最後のボックスであれば余りのテキストを表示
-        if (i === hunterListCreateElem.length - 1) {
-          return (
-            <HunterList
-              key={i}
-              class="w-full flex flex-col items-start justify-start text-white text-xs border border-solid border-blue-600 bg-white bg-opacity-60"
-              style={hunterListStyles}
-              innerHTML={{ __html: divisionRemaindertext }}></HunterList>
-          );
-        } else {
-          return (
-            <HunterList
-              key={i}
-              class="w-full flex items-start justify-center text-white text-xs border border-solid border-blue-600 bg-gray-200 bg-opacity-60"
-              style={hunterListStyles}></HunterList>
-          );
-        }
-      });
-    } else {
-      return <HunterList></HunterList>;
-    }
-  };
+  }
 
   // 高さの余りが910でなければ
   if (divisionRemainderHeightValue !== 910) {
@@ -169,59 +133,300 @@ export const Index: NextPage = () => {
     divisionRemainderBoxWidthPosition = 0;
   }
 
-  const HunterHuunter = () => {
-    if (divisionRemainderHeight === 0 && divisionRemainderWidth === 0) {
-      return (
-        <NextImg
-          class="text-center mb-5"
-          src="/images/xgjhaa.webp"
-          width="586"
-          height="369"></NextImg>
-      );
+  const ShowHunterList = (props) => {
+    const isShow: Boolean = props.isShow;
+    const isTextHidden: Boolean = props.isTextHidden;
+    if (isShow) {
+      return hunterListCreateElem.map((key: any, index: number) => {
+        // 偶数列と最後のボックス以外は余りのテキストを出さない
+        if (isTextHidden || index !== hunterListCreateElem.length - 1) {
+          return (
+            // <HunterList key={key.id} style={hunterListStyles} />;
+            <span key={key.id}></span>
+          );
+        } else {
+          return (
+            <HunterList
+              key={key.id}
+              // ref={props.ref}
+              class="justify-start bg-white flex-col"
+              style={hunterListStyles}
+              innerHTML={{ __html: divisionRemainderText }}
+            />
+          );
+        }
+      });
     } else {
-      return (
-        <NextImg
-          class="text-center mb-5"
-          src="/images/20170515114123.jpg"
-          width="640"
-          height="480"></NextImg>
-      );
+      return <span></span>;
     }
   };
 
-  // const LoopCreate: any = () => {
-  //   return loopCount.map((index, key) => {
-  //     return (
-  //       <HunterListWrap
-  //         key={key}
-  //         classPositionLeft={`left-${index - 1}/4`}
-  //         styleWidth={hunterListWrapBoxWidth}
-  //         styleTop="0"
-  //         // styleLeft={divisionRemainderBoxWidthPosition}
-  //       >
-  //         <ShowHunterList isShow={simulationFlag} />
-  //         <HunterEvenRows
-  //           styleHeightandTop={divisionRemainderBoxHeightandTopPosition}
-  //           text={divisionRemainderHeightValueText}
-  //           isShow={simulationFlag}
-  //         />
-  //       </HunterListWrap>
-  //     );
-  //   });
+  // HunterListボックス生成
+  // const ShowHunterList = (props) => {
+  //   const isShow: Boolean = props.isShow;
+  //   const isTextHidden: Boolean = props.isTextHidden;
+  //   if (isShow) {
+  //     return hunterListCreateElem.map((key: any, index: number) => {
+  //       // 偶数列は余りのテキストを出さない処理
+  //       if (isTextHidden) {
+  //         return <HunterList key={key} style={hunterListStyles}></HunterList>;
+  //       }
+
+  //       // HunterListの最後のボックスであれば余りのテキストを表示
+  //       if (key === hunterListCreateElem.length - 1) {
+  //         return (
+  //           <HunterList
+  //             key={key}
+  //             class="justify-start bg-white flex-col"
+  //             style={hunterListStyles}
+  //             innerHTML={{ __html: divisionRemainderText }}></HunterList>
+  //         );
+  //       } else {
+  //         return <HunterList key={key} style={hunterListStyles}></HunterList>;
+  //       }
+  //     });
+  //   } else {
+  //     return <span></span>;
+  //   }
   // };
+
+  // const pra: any = useRef(null);
+  // console.log(pra.current);
+  // pra.ondragstart = function (e) {
+  //   return false;
+  // };
+  // const onmousedown = (event) => {
+  //   document.addEventListener('mousemove', onMouseMove);
+  // };
+  // var onMouseMove = (event) => {
+  //   var x = event.clientX;
+  //   var y = event.clientY;
+  //   var width = pra.current.offsetWidth;
+  //   var height = pra.current.offsetHeight;
+  //   pra.current.style.position = 'absolute';
+  //   pra.current.style.top = y - height / 2 + 'px';
+  //   pra.current.style.left = x - width / 2 + 'px';
+  // };
+  // console.log(arrayObj);
+
+  ///// ドラッグ処理 ///////
+  const aaa = [
+    {
+      asasas: 'aaaaa',
+    },
+    {
+      asasas: 'bbb',
+    },
+    {
+      asasas: 'ccc',
+    },
+    {
+      asasas: 'dddd',
+    },
+    {
+      asasas: 'fff',
+    },
+    {
+      asasas: 'eee',
+    },
+    {
+      asasas: 'qqq',
+    },
+    {
+      asasas: 'lll',
+    },
+  ];
+  const onDragEnd = (result) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+  };
+  resetServerContext();
+  renderToString();
+
+  ///// ループテスト ///////
+  const LoopObj = [
+    {
+      id: Math.floor(Math.random() * 10001),
+      classes: 'h-full w-3/12 absolute',
+      styles: [
+        {
+          width: `${hunterListWrapBoxWidth}%`,
+          top: `0`,
+          left: `-${divisionRemainderBoxWidthPosition}%`,
+        },
+      ],
+      textHidden: false,
+      hunterEvenRows: false,
+    },
+    {
+      id: Math.floor(Math.random() * 10001),
+      classes: 'h-full w-3/12 absolute left-1/4',
+      styles: [
+        {
+          width: `${hunterListWrapBoxWidth}%`,
+          top: `${divisionRemainderBoxHeightandTopPosition}%`,
+          left: `${
+            hunterListWrapBoxWidth - divisionRemainderBoxWidthPosition
+          }%`,
+        },
+      ],
+      textHidden: true,
+      hunterEvenRows: true,
+    },
+    {
+      id: Math.floor(Math.random() * 10001),
+      classes: 'h-full w-3/12 absolute left-2/4',
+      styles: [
+        {
+          width: `${hunterListWrapBoxWidth}%`,
+          top: `0%`,
+          left: `${
+            hunterListWrapBoxWidth * 2 - divisionRemainderBoxWidthPosition
+          }%`,
+        },
+      ],
+      textHidden: false,
+      hunterEvenRows: false,
+    },
+    {
+      id: Math.floor(Math.random() * 10001),
+      classes: 'h-full w-3/12 absolute left-3/4',
+      styles: [
+        {
+          width: `${hunterListWrapBoxWidth}%`,
+          top: `${divisionRemainderBoxHeightandTopPosition}%`,
+          left: `${
+            hunterListWrapBoxWidth * 3 - divisionRemainderBoxWidthPosition
+          }%`,
+        },
+      ],
+      textHidden: true,
+      hunterEvenRows: true,
+    },
+  ];
+  // console.log(LoopObj);
+  // console.log(ref);
+  // const lalkk = React.createRef();
+  // console.log(lalkk);
 
   return (
     <Layout home={false}>
       <Head>
         <title>ハンターハンター</title>
       </Head>
+
       <section className="pb-20 mb-28 overflow-y-auto">
         <HunterWidthResult
           divisionRemainderWidthValue={divisionRemainderWidthValue}
         />
-        <div className="relative flex h-80 border-2 border-solid border-gray-500">
-          {/* <LoopCreate /> */}
-          <div
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable key={1} droppableId="test">
+            {(provided) => (
+              <div
+                className="relative flex h-80 border-2 border-solid border-gray-500"
+                {...provided.droppableProps}
+                ref={provided.innerRef}>
+                {LoopObj.map((key, index) => {
+                  if (!key.hunterEvenRows) {
+                    return (
+                      <div
+                        key={key.id}
+                        className={key.classes}
+                        style={{
+                          width: key.styles[0].width,
+                          top: key.styles[0].top,
+                          left: key.styles[0].left,
+                        }}>
+                        {hunterListCreateElem.map((keys: any, index: any) => {
+                          return (
+                            <Draggable
+                              draggableId={String(keys.id + key.id)}
+                              key={keys.id}
+                              index={index}>
+                              {(provided) => (
+                                // <HunterDiv
+                                //   class="text-red-600"
+                                //   provided={provided}
+                                //   // {...provided.draggableProps}
+                                //   // {...provided.dragHandleProps}
+                                //   ref={provided.innerRef}>
+                                //   {keys.id}
+                                // </HunterDiv>
+                                <HunterList
+                                  style={hunterListStyles}
+                                  ref={provided.innerRef}
+                                  provided={provided}
+                                />
+                                // <p
+                                //   // key={keys}
+                                //   ref={provided.innerRef}
+                                //   {...provided.draggableProps}
+                                //   {...provided.dragHandleProps}
+                                //   // isShow={simulationFlag}
+                                // >
+                                //   <span>{keys.id}</span>
+                                // </p>
+                              )}
+                            </Draggable>
+                          );
+                        })}
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        key={key.id}
+                        className={key.classes}
+                        style={{
+                          width: key.styles[0].width,
+                          top: key.styles[0].top,
+                          left: key.styles[0].left,
+                        }}>
+                        {hunterListCreateElem.map((keys: any, num: any) => {
+                          // console.log(num);
+
+                          return (
+                            <>
+                              <Draggable
+                                draggableId={String(keys.id + key.id)}
+                                key={keys.id}
+                                index={num}>
+                                {(provided) => (
+                                  <HunterList
+                                    style={hunterListStyles}
+                                    ref={provided.innerRef}
+                                    provided={provided}
+                                  />
+                                )}
+                              </Draggable>
+                              <HunterEvenRows
+                                styleHeightandTop={
+                                  divisionRemainderBoxHeightandTopPosition
+                                }
+                                text={divisionRemainderHeightValueText}
+                                isShow={simulationFlag}
+                              />
+                            </>
+                          );
+                        })}
+                        {/* <ShowHunterList
+                          isShow={simulationFlag}
+                          isTextHidden={key.textHidden}
+                        />
+                        <HunterEvenRows
+                          styleHeightandTop={
+                            divisionRemainderBoxHeightandTopPosition
+                          }
+                          text={divisionRemainderHeightValueText}
+                          isShow={simulationFlag}
+                        /> */}
+                      </div>
+                    );
+                  }
+                })}
+                {/* <div
             className="h-full w-3/12 absolute"
             style={{
               width: `${hunterListWrapBoxWidth}%`,
@@ -270,10 +475,17 @@ export const Index: NextPage = () => {
               text={divisionRemainderHeightValueText}
               isShow={simulationFlag}
             />
-          </div>
-        </div>
+          </div> */}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </section>
-      <HunterHuunter />
+      <HunterHunter
+        widthValue={divisionRemainderWidth}
+        heightValue={divisionRemainderHeight}
+      />
       <div className="flex space-x-4 mb-5">
         <TextInput
           label="幅(mm)"
@@ -288,6 +500,36 @@ export const Index: NextPage = () => {
           value={heightInputText}
         />
       </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="sample">
+          {(provided) => (
+            <div
+              className="bg-blue-400 mb-7 h-auto"
+              {...provided.droppableProps}
+              ref={provided.innerRef}>
+              {aaa.map((key, index) => {
+                return (
+                  <Draggable
+                    draggableId={key.asasas}
+                    key={key.asasas}
+                    index={index}>
+                    {(provided) => (
+                      <p
+                        className="mb-2 p-5 bg-yellow-200"
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}>
+                        {key.asasas}
+                      </p>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
       <HunterBothResult
         widthInputText={widthInputText}
         heightInputText={heightInputText}
