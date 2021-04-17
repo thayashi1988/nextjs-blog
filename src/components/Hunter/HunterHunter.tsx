8bed87cbd79151e71d5d8f5af9402eeeb7d837a1
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2021-04-17 16:53:30 +0900

    💄 画像表示をコンポーネント化

---
+++ src/components/Hunter/HunterHunter.tsx
@@ -0,0 +1,31 @@
+// react
+import type { VFC } from 'react';
+import React from 'react';
+
+// components
+import { NextImg } from 'src/components/Img/Img';
+
+type PROPS = {
+  heightValue: number;
+  widthValue: number;
+};
+
+export const HunterHunter: VFC<PROPS> = (props) => {
+  if (props.heightValue === 0 && props.widthValue === 0) {
+    return (
+      <NextImg
+        class="text-center mb-5"
+        src="/images/xgjhaa.webp"
+        width="586"
+        height="369"></NextImg>
+    );
+  } else {
+    return (
+      <NextImg
+        class="text-center mb-5"
+        src="/images/20170515114123.jpg"
+        width="640"
+        height="480"></NextImg>
+    );
+  }
+};
