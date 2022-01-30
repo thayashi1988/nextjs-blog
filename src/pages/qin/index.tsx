5d17007b323a972b0d70263b7d1c35980f9af1cf
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-01-30 16:53:16 +0900

    ♻️ add types qin page

--- src/pages/qin/index.tsx
+++ src/pages/qin/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next';
 import React from 'react';
 import Head from 'next/head';
+import { Btn } from '@/components/Button/Btn';
 
-export const Index: NextPage = () => {
+const Index: NextPage = () => {
   return (
     <>
       <Head>
         <title>React講座</title>
       </Head>
+      <Btn link={false} class="bg-green-500">
+        あああああああああああ
+      </Btn>
     </>
   );
 };
