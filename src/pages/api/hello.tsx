8fe79ad90f02754d3e3b6d235a9c6e161d82f725
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-01-30 15:46:41 +0900

    api 修正

--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -1,3 +1,11 @@
-export default (req, res) => {
-  res.status(200).json({ text: 'API Hello' });
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  name: string;
 };
+
+const hello = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.status(200).json({ name: 'John Doe' });
+};
+
+export default hello;
