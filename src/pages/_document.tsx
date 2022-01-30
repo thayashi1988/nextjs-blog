e5974c7c694865f53b8a307a3a6a7ef00842a973
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-01-30 19:16:53 +0900

    ♻️ _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,19 +2,33 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="ja">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+const MyDocument = () => {
+  return (
+    <Html lang="ja-JP">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default MyDocument;
+
+// export default class MyDocument extends Document {
+//   render() {
+//     return (
+//       <Html lang="ja">
+//         <Head />
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
 
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with server-side generation (SSG).
@@ -47,7 +61,8 @@ MyDocument.getInitialProps = async (ctx) => {
 
   ctx.renderPage = () =>
     originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      enhanceApp: (App: any) => (props: any) =>
+        sheets.collect(<App {...props} />),
     });
 
   const initialProps = await Document.getInitialProps(ctx);
