a35427a38bd693ecf8bb429d7e29c6e32a3b10b0
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-01-31 23:42:23 +0900

    🎨 カレントがつくように処理調整

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,5 @@
 import { NextLink } from '@/components/Link/Link';
+import { useRouter } from 'next/router';
 import type { VFC } from 'react';
 import React from 'react';
 
@@ -6,24 +7,37 @@ const NavItems = [
   {
     href: '/qin/posts',
     txt: 'Posts',
+    current: false,
   },
   {
     href: '/qin/users',
     txt: 'Users',
+    current: false,
   },
   {
     href: '/qin/comments',
     txt: 'Comments',
+    current: false,
   },
 ];
 
 export const Nav: VFC = () => {
+  const router = useRouter();
+  NavItems.forEach((item) => {
+    if (item.href === router.asPath) {
+      item.current = true;
+    } else {
+      item.current = false;
+    }
+  });
+  console.log('router.asPath:', router.asPath);
+
   return (
     <nav className="w-3/5 mx-auto">
       <ul className="flex items-center justify-between">
         {NavItems.map((item) => {
           return (
-            <li key={item.href}>
+            <li key={item.href} className={item.current ? 'bg-yellow-300' : ''}>
               <NextLink href={item.href} class="text-2xl font-bold">
                 {item.txt}
               </NextLink>
