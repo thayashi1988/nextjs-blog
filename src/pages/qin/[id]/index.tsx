333a208e196dba9c9a6e3059f8f28c8603039501
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-01-31 21:56:33 +0900

    🚚 動的ルーティングのディレクトリ変更

---
+++ src/pages/qin/[id]/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import { Btn } from '@/components/Button/Btn';
+import { Post } from '@/components/Post';
+
+const PostId: NextPage = () => {
+  return (
+    <>
+      <Post />
+      <Btn link href="/qin" class="block mt-4">
+        前のページに戻る
+      </Btn>
+    </>
+  );
+};
+
+export default PostId;
