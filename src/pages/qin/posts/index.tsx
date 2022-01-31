945203ea20cc6ab912512e9b73ac6e207468ef3b
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-01-31 23:25:37 +0900

    🚚 投稿ページのディレクトリを変更

---
+++ src/pages/qin/posts/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import Head from 'next/head';
+import { Nav } from '@/components/Nav/Nav';
+import { RestApiPost } from '@/components/RestApiPost';
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>React講座</title>
+      </Head>
+      <Nav />
+      <RestApiPost />
+    </>
+  );
+};
+
+export default Index;
