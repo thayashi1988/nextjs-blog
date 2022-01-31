1effadf861eb99fb588072e88605d1b83c2d9272
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-01-31 14:33:18 +0900

    swrでfetch

--- src/components/RestApiPost/index.tsx
+++ src/components/RestApiPost/index.tsx
@@ -1,6 +1,7 @@
 import type { VFC } from 'react';
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 import { NextLink } from '@/components/Link/Link';
+import { usePosts } from '@/components/Hooks/usePosts';
 
 // サーバーサイドレンダリング時に使用する（静的ジェネレーターの場合で使用）
 // export async function getStaticProps() {
@@ -15,31 +16,64 @@ import { NextLink } from '@/components/Link/Link';
 //   };
 // }
 
+// const initialState = {
+//   posts: [],
+//   loading: true,
+//   error: null,
+// };
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'end':
+//       return {
+//         ...state,
+//         posts: action.posts,
+//         loading: false,
+//       };
+//     case 'error':
+//       return {
+//         ...state,
+//         error: action.error,
+//         loading: false,
+//       };
+
+//     default:
+//       throw new Error('エラー');
+//   }
+// };
+type apiProps = {
+  id: string;
+  link: string;
+  title: {
+    rendered: string;
+  };
+};
+
 export const RestApiPost: VFC = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const { data, error, isLoading, isEmpty } = usePosts(
+    `${process.env.SAKURA_REST_API}`
+  );
+  // const [state, dispatch] = useReducer(reducer, initialState);
 
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch(`${process.env.SAKURA_REST_API}`);
-      // https://jsonplaceholder.typicode.com/posts
-      if (!res.ok) {
-        throw new Error(res.statusText);
-      }
-      const data = await res.json();
-      setPosts(data);
-    } catch (error) {
-      setError(true);
-    }
-    setLoading(false);
-  }, []);
+  // const getPosts = useCallback(async () => {
+  //   try {
+  //     const res = await fetch(`${process.env.SAKURA_REST_API}`);
+  //     // https://jsonplaceholder.typicode.com/posts
+  //     if (!res.ok) {
+  //       throw new Error(res.statusText);
+  //     }
+  //     const data = await res.json();
+  //     dispatch({ type: 'end', posts: data });
+  //   } catch (error) {
+  //     dispatch({ type: 'error', error: error });
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
+  // useEffect(() => {
+  //   getPosts();
+  // }, [getPosts]);
 
-  if (loading) {
+  if (isLoading) {
     return (
       <div className="animate-pulse mt-5">
         <div className="h-3 w-3/5 mb-3 bg-gray-400 rounded"></div>
@@ -49,16 +83,16 @@ export const RestApiPost: VFC = () => {
     );
   }
   if (error) {
-    return <div>error...</div>;
+    return <div>{error.message}</div>;
   }
-  if (posts.length === 0) {
+  if (isEmpty) {
     return <div>データはからです...</div>;
   }
-  console.log('posts:', posts);
+  console.log('data:', data);
 
   return (
     <ul className="my-5">
-      {posts.map((post) => {
+      {data.map((post: apiProps) => {
         return (
           <li className="mb-3 text-sm" key={post.id}>
             <NextLink href={post.link} class="text-blue-800 hover:opacity-80">
