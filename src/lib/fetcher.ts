6abba194cf4c719a8c5c67d35fdee3b87c7d4397
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-01 14:35:28 +0900

    ♻️ エラー文言修正

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,7 +1,7 @@
 export const fetcher = async (url: string) => {
   const response = await fetch(url);
   if (!response.ok) {
-    throw new Error('エラー発生しました。');
+    throw new Error('エラーが発生しました。');
   }
   const json = await response.json();
   return json;
