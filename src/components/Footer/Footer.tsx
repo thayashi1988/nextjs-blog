73520f9d1bd7f076a6165d83236f5470ee954b9f
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 01:16:53 +0900

    ✨ footerの高さを取得してfooterを画面下に配置

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import type { VFC } from 'react';
 
 export const Footer: VFC = () => {
   const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-blue-300 px-2 py-5">
       <p className="text-center text-sm">
--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { Header } from 'src/components/Header/Header';
@@ -6,33 +6,50 @@ import { Footer } from 'src/components/Footer/Footer';
 import { Btn } from 'src/components/Button/Btn';
 import styles from 'src/components/Layout/layout.module.scss';
 
-export const siteTitle: string = 'Next.jsアプリ';
-export const siteDescription: string = 'Next.js Sample Website';
-export const siteUrl: string = 'http://localhost:3000/';
+export const siteSettings = {
+  siteTitle: 'Next.jsアプリ',
+  siteDescription: 'Next.js Sample Website',
+  siteUrl: 'https://nextjs-sample-app.vercel.app',
+};
 
 export const Layout = ({ children }) => {
+  const [footerHeight, setFooterHeight] = useState(0);
+  const footerElem = useRef();
+
   const router = useRouter();
   const { pathname } = router;
   const isHome = pathname === '/';
 
+  useEffect(() => {
+    const target: any = footerElem.current;
+    const getHeight = target.getBoundingClientRect();
+    console.log('footerHeight:', footerHeight);
+    setFooterHeight((prev) => getHeight.height);
+  }, [footerHeight]);
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <div className={styles.container}>
-        <Head>
-          <meta name="description" content={siteDescription} />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content={siteTitle} />
-          <meta property="og:url" content={siteUrl} />
-          <meta property="og:description" content={siteDescription} />
-          <meta property="og:site_name" content={siteTitle} />
-          <meta property="og:image" content={`${siteUrl}/ogp.png`} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="format-detection" content="telephone=no" />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        </Head>
-        <main className="mt-5">{children}</main>
+    <>
+      <Head>
+        <meta name="description" content={siteSettings.siteDescription} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={siteSettings.siteTitle} />
+        <meta property="og:url" content={siteSettings.siteUrl} />
+        <meta
+          property="og:description"
+          content={siteSettings.siteDescription}
+        />
+        <meta property="og:site_name" content={siteSettings.siteTitle} />
+        <meta property="og:image" content={`${siteSettings.siteUrl}/ogp.png`} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="format-detection" content="telephone=no" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+
+      <div
+        className="flex flex-col min-h-screen"
+        style={{ minHeight: `calc(100vh - ${footerHeight}px)` }}>
+        <Header />
+        <main className="flex-1 my-6">{children}</main>
         {!isHome ? (
           <div className={styles.backToHome}>
             <Btn link href="/">
@@ -41,8 +58,10 @@ export const Layout = ({ children }) => {
           </div>
         ) : null}
       </div>
-      <Footer />
-    </div>
+      <div ref={footerElem}>
+        <Footer />
+      </div>
+    </>
   );
 };
 export default memo(Layout);
