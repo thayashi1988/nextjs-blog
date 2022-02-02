6453307c0a9302913172f0a13b0c749b87e6662c
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 00:15:41 +0900

    ♻️ フッターコピーライトにサイトタイトル追加、headerと同じpaddingを追加

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,10 @@ import type { VFC } from 'react';
 export const Footer: VFC = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="bg-blue-300 p-2 mt-auto">
-      <p className="text-center text-sm">&copy; 2021~{currentYear}</p>
+    <footer className="bg-blue-300 px-2 py-5">
+      <p className="text-center text-sm">
+        &copy; Next.jsアプリ 2021~{currentYear}
+      </p>
     </footer>
   );
 };
