b854b96ee7695b9ed47b4edadcb421743bcd3cfe
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 23:37:06 +0900

    ✨ ログイン時に表示するローディングページ作成

---
+++ src/pages/loading/index.tsx
@@ -0,0 +1,9 @@
+import { LoadingTop } from '@/components/Loading/LoadingTop';
+import { NextPage } from 'next';
+import React from 'react';
+
+const index: NextPage = () => {
+  return <LoadingTop />;
+};
+
+export default index;
