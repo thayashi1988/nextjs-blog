8a31f950efaa6c0b0ae68b08af4da1b1b676b6dd
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 11:29:09 +0900

    ⚡️ ログイン後ページ製作途中

---
+++ src/pages/[id]/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import Head from 'next/head';
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>firebaseでのGoogle認証</title>
+      </Head>
+    </>
+  );
+};
+
+export default Index;
