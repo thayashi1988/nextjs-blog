db2b27b8760c03084e9173a7705991ec6afe59e4
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 21:26:40 +0900

    💄 ローディングコンポーネントにborderつけた

--- src/components/Loading/LoadingBox.tsx
+++ src/components/Loading/LoadingBox.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const LoadingBox: React.VFC = () => {
   return (
     <div
-      className={`shadow-gray-300/50 rounded overflow-hidden shadow-lg sm:max-w-xl mx-auto p-4 sm:p-8`}>
+      className={`border border-gray-100 shadow-gray-300/50 rounded overflow-hidden shadow-lg sm:max-w-xl mx-auto p-4 sm:p-8`}>
       <div className="animate-pulse mt-5">
         <div className="h-3 w-3/6 mb-8 bg-gray-400 rounded text-center mx-auto"></div>
         <div className="h-3 w-4/6 mb-3 bg-gray-400 rounded"></div>
--- src/components/Loading/LoadingText.tsx
+++ src/components/Loading/LoadingText.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 export const LoadingText: React.VFC = () => {
   return (
-    <div className="animate-pulse mt-5">
-      <div className="h-3 w-6/6 mb-3 bg-gray-400 rounded"></div>
-      <div className="h-3 w-2/6 mb-6 bg-gray-400 rounded"></div>
-      <div className="h-3 w-6/6 mb-3 bg-gray-400 rounded"></div>
-      <div className="h-3 w-2/6 mb-3 bg-gray-400 rounded"></div>
+    <div className="border border-gray-100 shadow rounded-md p-4 w-full">
+      <div className="animate-pulse mt-5">
+        <div className="h-3 w-6/6 mb-3 bg-gray-400 rounded"></div>
+        <div className="h-3 w-2/6 mb-6 bg-gray-400 rounded"></div>
+        <div className="h-3 w-6/6 mb-3 bg-gray-400 rounded"></div>
+        <div className="h-3 w-2/6 mb-3 bg-gray-400 rounded"></div>
+      </div>
     </div>
   );
 };
