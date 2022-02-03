e0c0f7dfbda5ab52687552e34ad6381dcf06b1d3
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 17:02:24 +0900

    ♻️ 型修正、ボタン変更、カレント修正

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,7 @@
+import React from 'react';
+import { BtnInfo } from '@/components/Button/BtnInfo';
 import { NextLink } from '@/components/Link/Link';
 import { useRouter } from 'next/router';
-import type { VFC } from 'react';
-import React from 'react';
 
 const NavItems = [
   {
@@ -21,7 +21,7 @@ const NavItems = [
   },
 ];
 
-export const Nav: VFC = () => {
+export const Nav: React.VFC = () => {
   const router = useRouter();
   NavItems.forEach((item) => {
     if (item.href === router.asPath) {
@@ -33,11 +33,22 @@ export const Nav: VFC = () => {
 
   return (
     <nav className="max-w-screen-md w-full mx-auto">
+      <BtnInfo
+        link
+        href="/qin"
+        margin="text-cener"
+        class="text-center sm:max-w-full">
+        動的ルーティング・連続fetchのTOPへ戻る
+      </BtnInfo>
       <ul className="flex items-center justify-between">
         {NavItems.map((item) => {
           return (
-            <li key={item.href} className={item.current ? 'bg-yellow-300' : ''}>
-              <NextLink href={item.href} class="text-2xl font-bold">
+            <li key={item.href}>
+              <NextLink
+                href={item.href}
+                class={`text-lg sm:text-2xl font-bold p-4 ${
+                  item.current ? 'bg-yellow-200' : ''
+                }`}>
                 {item.txt}
               </NextLink>
             </li>
