53662b0d32913e8a8d4347c3b68d8a0208bd69d0
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 21:05:44 +0900

    💄 ローディングコンポーネントのテキスト部分を少し伸ばす

--- src/components/Loading/LoadingBox.tsx
+++ src/components/Loading/LoadingBox.tsx
@@ -6,10 +6,10 @@ export const LoadingBox: React.VFC = () => {
       className={`shadow-gray-300/50 rounded overflow-hidden shadow-lg sm:max-w-xl mx-auto p-4 sm:p-8`}>
       <div className="animate-pulse mt-5">
         <div className="h-3 w-3/6 mb-8 bg-gray-400 rounded text-center mx-auto"></div>
-        <div className="h-3 w-2/6 mb-3 bg-gray-400 rounded"></div>
-        <div className="h-3 w-2/6 mb-3 bg-gray-400 rounded"></div>
-        <div className="h-3 w-2/6 mb-3 bg-gray-400 rounded"></div>
-        <div className="h-3 w-2/6 bg-gray-400 rounded"></div>
+        <div className="h-3 w-4/6 mb-3 bg-gray-400 rounded"></div>
+        <div className="h-3 w-4/6 mb-3 bg-gray-400 rounded"></div>
+        <div className="h-3 w-4/6 mb-3 bg-gray-400 rounded"></div>
+        <div className="h-3 w-4/6 bg-gray-400 rounded"></div>
       </div>
     </div>
   );
