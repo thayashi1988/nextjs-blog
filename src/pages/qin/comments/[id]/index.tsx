dc3df8083e767c2b73a071d58e75ad1915fb6ae8
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 18:52:59 +0900

    ♻️ 説明追加、型修正

--- src/pages/qin/comments/[id]/index.tsx
+++ src/pages/qin/comments/[id]/index.tsx
@@ -1,15 +1,36 @@
 import type { NextPage } from 'next';
 import React from 'react';
-import { Btn } from '@/components/Button/Btn';
+import Head from 'next/head';
 import { Comment } from '@/components/Qin/Comment';
+import { Nav } from '@/components/Nav/Nav';
+import { Text } from '@/components/Text/Text';
+import { Heading1 } from '@/components/Heading/Heading1';
+import { NextLink } from '@/components/Link/Link';
 
 const commentsId: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>
+          commentAPI連続fetch | commentAPI表示 | 動的ルーティング・連続fetch |
+          Next.jsアプリ
+        </title>
+      </Head>
+      <Heading1>commentAPI連続fetch</Heading1>
+      <Text>
+        このページで前のページで取得したidとuserIdを元に動的ルーティングを行っております。
+        <br />
+        例）/qin/users で10番目のリンクを叩くと、/qin/users/10
+        と前のページで取得したidをページディレクトリとしてルーティングさせています。
+      </Text>
+      <Text class="mb-0">
+        また、そのidを用いて以下のAPをさらにたたき、idに紐付いたphotos情報を取得し、表示させています。
+      </Text>
+      <NextLink target href="https://jsonplaceholder.typicode.com/photos">
+        {`https://jsonplaceholder.typicode.com/photos?id={id}`}
+      </NextLink>
+      <Nav />
       <Comment />
-      <Btn link href="/qin" class="block mt-4">
-        React講座へ戻る
-      </Btn>
     </>
   );
 };
--- src/pages/qin/comments/index.tsx
+++ src/pages/qin/comments/index.tsx
@@ -2,14 +2,26 @@ import type { NextPage } from 'next';
 import React from 'react';
 import Head from 'next/head';
 import { Nav } from '@/components/Nav/Nav';
+import { Text } from '@/components/Text/Text';
+import { Heading1 } from '@/components/Heading/Heading1';
+import { NextLink } from '@/components/Link/Link';
 import { Comments } from '@/components/Qin/Comments';
 
 const Index: NextPage = () => {
   return (
     <>
       <Head>
-        <title>React講座 commentsを表示</title>
+        <title>
+          commentsAPI表示 | 動的ルーティング・連続fetch | Next.jsアプリ
+        </title>
       </Head>
+      <Heading1>commentsAPI表示</Heading1>
+      <Text class="mb-0">
+        このページでは以下のAPIを叩きデータを取得し、id、nameを表示させています。
+      </Text>
+      <NextLink target href="https://jsonplaceholder.typicode.com/comments">
+        https://jsonplaceholder.typicode.com/comments
+      </NextLink>
       <Nav />
       <Comments />
     </>
