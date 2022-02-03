049e2bbc01ff893f4ef9dad6ad2f22d0beed46ee
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 13:18:51 +0900

    ♻️ カラムコンポーネント640px〜768px内の表示切り替えprops追加

--- src/components/Column/ColumnItem.tsx
+++ src/components/Column/ColumnItem.tsx
@@ -3,16 +3,18 @@ import React from 'react';
 
 type PROPS = {
   col: string;
+  colSm: string;
   class?: string;
   children: React.ReactNode;
 };
 
 export const ColumnItem: VFC<PROPS> = (props) => {
-  console.log('props.col:', `md:w-${props.col}/12`);
+  // console.log('props.col:', `md:w-${props.col}/12`);
   const addColumnClass = props.class ? ` ${props.class}` : '';
 
   return (
-    <div className={`w-full md:w-${props.col}/12 px-2 pt-2${addColumnClass}`}>
+    <div
+      className={`w-full sm:w-${props.colSm}/6 md:w-${props.col}/12 px-2 pt-2${addColumnClass}`}>
       {/* <div className={`w-full md:w-${props.col}/12 px-2 pt-2 ${props.class}`}> */}
       {props.children}
     </div>
