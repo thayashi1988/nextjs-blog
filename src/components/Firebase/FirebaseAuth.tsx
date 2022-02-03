22d20265a5792d7757847d98104fd8992195c41e
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 11:26:55 +0900

    ♻️ 認証コンポーネント修正

--- src/components/Firebase/FirebaseAuth.tsx
+++ src/components/Firebase/FirebaseAuth.tsx
@@ -1,12 +1,29 @@
-import { auth } from '../../../firebase';
+// import React, { useState, useEffect } from 'react';
+import { fbApp, auth } from '../../../firebase';
 
 // ログアウト機能
 export const handleLogOut = (): any => {
   return auth.signOut();
 };
 
+// ログイン機能
+export const handleLogIn = (): any => {
+  const provider = new fbApp.auth.GoogleAuthProvider();
+  return auth.signInWithRedirect(provider);
+};
+
 // ログイン情報取得機能
-// export const authProcess = auth.onAuthStateChanged((firebaseDatas: any) => {
-//   console.log('firebaseDatas:', firebaseDatas);
-//   return firebaseDatas;
-// });
+// export const GetAuthInfo = () => {
+//   // ログイン情報
+//   const [user, setUser] = useState(null);
+
+//   //ボタン活性化
+//   useEffect(() => {
+//     const authProcess = auth.onAuthStateChanged((userInfo: any) => {
+//       setUser(userInfo);
+//     });
+//     return () => authProcess();
+//   }, []);
+//   console.log('user:', user);
+//   return user;
+// };
