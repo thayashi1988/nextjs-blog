7033a621aa33496577309accbe4f2f607f96e336
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 13:20:25 +0900

    ♻️ ハンターハンターでNaNになるのを修正

--- src/components/Hunter/HunterBothResult.tsx
+++ src/components/Hunter/HunterBothResult.tsx
@@ -18,32 +18,37 @@ const settingBordNum = (inputNum: any, type: string): number => {
 };
 
 export const HunterBothResult: VFC<PROPS> = (props) => {
+  const displayWidthNum =
+    props.widthInputText === '' ? '0' : props.widthInputText;
+  const displayHeightNum =
+    props.heightInputText === '' ? '0' : props.heightInputText;
+  console.log('displayWidthNum:', displayWidthNum);
   return (
     <div className="sm:flex sm:space-x-4 mb-5">
       <div className="border rounded-lg shadow-md w-full bg-blue-100 p-5 mb-4 dark:text-black">
         <h3 className="font-bold sm:text-lg text-sm mb-4 break-all">
-          幅：{props.widthInputText}
+          幅：{displayWidthNum}
           <sub>mm</sub>
         </h3>
         <ul className="list-disc pl-5 break-all">
           <li>
             幅 ÷ 303<sub>mm</sub> ={' '}
             <span className="text-red-600 font-bold">
-              {settingBordNum(props.widthInputText, 'width')}
+              {settingBordNum(displayWidthNum, 'width')}
               枚の板を置けるよ
             </span>
           </li>
           <li>
             幅 ÷ 303<sub>mm</sub> の余り ={' '}
             <span className="text-red-600 font-bold">
-              {parseFloat(props.widthInputText) % 303}
+              {parseFloat(displayWidthNum) % 303}
               <sub>mm</sub>
             </span>
           </li>
           <li>
             幅 ÷ 303<sub>mm</sub>の余り ÷ 2={' '}
             <span className="text-red-600 font-bold">
-              {(parseFloat(props.widthInputText) % 303) / 2}
+              {(parseFloat(displayWidthNum) % 303) / 2}
               <sub>mm</sub>
             </span>
           </li>
@@ -51,28 +56,28 @@ export const HunterBothResult: VFC<PROPS> = (props) => {
       </div>
       <div className="border rounded-lg shadow-md w-full bg-blue-100 p-5 mb-4 dark:text-black">
         <h3 className="font-bold sm:text-lg text-sm mb-4 break-all">
-          高さ：{props.heightInputText}
+          高さ：{displayHeightNum}
           <sub>mm</sub>
         </h3>
         <ul className="list-disc pl-5 break-all">
           <li>
             高さ ÷ 1820<sub>mm</sub> ={' '}
             <span className="text-red-600 font-bold">
-              {settingBordNum(props.heightInputText, 'height')}
+              {settingBordNum(displayHeightNum, 'height')}
               枚の板を置けるよ
             </span>
           </li>
           <li>
             高さ ÷ 1820<sub>mm</sub> の余り ={' '}
             <span className="text-red-600 font-bold">
-              {parseFloat(props.heightInputText) % 1820}
+              {parseFloat(displayHeightNum) % 1820}
               <sub>mm</sub>
             </span>
           </li>
           <li>
             高さ ÷ 1820<sub>mm</sub>の余り ÷ 2={' '}
             <span className="text-red-600 font-bold">
-              {(parseFloat(props.heightInputText) % 1820) / 2}
+              {(parseFloat(displayHeightNum) % 1820) / 2}
               <sub>mm</sub>
             </span>
           </li>
