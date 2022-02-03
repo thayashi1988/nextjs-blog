ddc2973ecd6dfb51d101138ce5d7b3f29d3aadae
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-03 17:55:23 +0900

    ♻️ 型修正

--- src/components/Qin/Users/index.tsx
+++ src/components/Qin/Users/index.tsx
@@ -1,16 +1,15 @@
-import type { VFC } from 'react';
 import React from 'react';
 import { NextLink } from '@/components/Link/Link';
 import { useUsers } from '@/components/Hooks/useFetch';
 import { Loading } from '@/components/Loading/Loading';
 import { TextAlert } from '@/components/Text/TextAlert';
 
-type apiUsersProps = {
+type APIUSERSPROPS = {
   id?: string;
   name?: string;
 };
 
-export const Users: VFC = () => {
+export const Users: React.VFC = () => {
   const { data, error, isLoading } = useUsers();
 
   if (isLoading) {
@@ -20,13 +19,11 @@ export const Users: VFC = () => {
     return <TextAlert>{error.message}</TextAlert>;
   }
   return (
-    <ul className="my-5">
-      {data.map((user: apiUsersProps) => {
+    <ul>
+      {data.map((user: APIUSERSPROPS) => {
         return (
           <li className="mb-4 text-sm" key={user.id}>
-            <NextLink
-              href={`/qin/users/${user.id}`}
-              class="text-black-600 hover:opacity-80">
+            <NextLink href={`/qin/users/${user.id}`}>
               {user.id}&nbsp;
               {user.name}
             </NextLink>
