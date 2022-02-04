1eb85d268cfec69c3a9b3f8e495c3a80f811a893
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-04 23:21:22 +0900

    ♻️ 不要コード削除

--- src/pages/storage/index.tsx
+++ src/pages/storage/index.tsx
@@ -1,25 +1,16 @@
 import type { NextPage } from 'next';
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-import { FBTest, storage, storageRef } from '../../../firebase';
+import { FBTest, storageRef } from '../../../firebase';
 import { Text } from '@/components/Text/Text';
 import { Btn } from '@/components/Button/Btn';
 import { NextImg } from '@/components/Img/Img';
 import { Heading2 } from '@/components/Heading/Heading2';
-import { LoadingText } from '@/components/Loading/LoadingText';
 import { FirebaseStorageFiles } from '@/components/Firebase/FirebaseStorageFiles';
 import { FirebaseStorageDirectorys } from '@/components/Firebase/FirebaseStorageDirectorys';
 
-const imgRef = storageRef.child('images/img_01.png');
-
 const fileMetadata = {
   contentType: 'image/*',
-};
-
-const allDirDatas = {
-  root: {
-    path: '/',
-  },
 };
 
 export const Index: NextPage = (props) => {
@@ -40,20 +31,14 @@ export const Index: NextPage = (props) => {
       .then((res) => {
         res.prefixes.forEach((folderRef) => {
           underDirs.push(folderRef.name);
-          // allDirDatas.root[folderRef.name] = { files: '' };
-          // allDirDatas.root[folderRef.name].path = `${folderRef.name}`;
           res.items.forEach((itemRef) => {
             underItems.push(itemRef.name);
           });
-          // allDirDatas.root[folderRef.name].files = [underItems];
         });
-        // console.log('allDirDatas:', allDirDatas);
         setStorageDir([...underDirs]);
         setStorageDatas([...underItems]);
         setIsLoading(underItems.length === 0 && underDirs.length === 0);
         console.log('underDirs:', underDirs);
-        // console.log('underItems:', underItems);
-        // console.log('storageDatas:', storageDatas);
         console.log('storageDir:', storageDir);
         console.log('isLoading:', isLoading);
       })
@@ -78,23 +63,17 @@ export const Index: NextPage = (props) => {
     const uploadTask = storageRef
       .child('images/' + file.name)
       .put(file, fileMetadata);
-    // console.log('uploadTask:', uploadTask);
 
     uploadTask.on(
       'state_changed',
       (snapshot) => {
-        // console.log('snapshot:', snapshot);
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
         setProg(progress);
         switch (snapshot.state) {
           case FBTest.storage.TaskState.PAUSED: // or 'paused'
-            console.log('Upload is paused');
             break;
           case FBTest.storage.TaskState.RUNNING: // or 'running'
-            console.log('Upload is running');
-            // setProg((oldProgress) => oldProgress + 1);
             break;
         }
       },
@@ -126,8 +105,6 @@ export const Index: NextPage = (props) => {
         });
       }
     );
-    console.log('uploadTask', uploadTask);
-    console.log('file:', file);
   };
 
   const handleDirBackToTop = () => {
@@ -144,18 +121,13 @@ export const Index: NextPage = (props) => {
       .then((res) => {
         res.prefixes.forEach((folderRef) => {
           underDirs.push(folderRef.name);
-          // allDirDatas.root[folderRef.name] = { files: '' };
-          // allDirDatas.root[folderRef.name].path = `${folderRef.name}`;
           res.items.forEach((itemRef) => {
             underItems.push(itemRef.name);
           });
         });
-        // console.log('allDirDatas:', allDirDatas);
         setStorageDir([...underDirs]);
         setStorageDatas([...underItems]);
         setIsLoading(underItems.length === 0 && underDirs.length === 0);
-        console.log('storageDatas:', storageDatas);
-        console.log('storageDir:', storageDir);
       })
       .catch((error) => {
         alert('handleDirBackToTop エラーが発生しました。');
@@ -163,27 +135,17 @@ export const Index: NextPage = (props) => {
       });
   };
 
-  const handleDirSearch = (e) => {
+  const handleDirSearch = (e: any) => {
     setStorageDatas([]);
     setStorageDir([]);
     setIsLoading(true);
 
     const clickedDir = e.target.value;
-    // console.log('clickedDir:', clickedDir);
     if (/\//.test(oldDir)) {
       setOldDir(`${clickedDir}`);
     } else {
       setOldDir((prev) => `${prev}/${clickedDir}`);
     }
-    // const hhhh = Object.keys(allDirDatas.root[oldDir]);
-    // const hhhh = Object.entries(allDirDatas).find(([key, index]) => {
-    //   console.log('key:', key);
-    //   // key[clickedDir] === clickedDir
-    // });
-    // console.log('hhhh:', hhhh);
-    // console.log('oldDir:', oldDir);
-    // const jjjj = `${oldDir}/${clickedDir}`;
-    // console.log('jjjj:', jjjj);
     const imgRootRef = storageRef.child(`${clickedDir}`);
     const underItems = [];
     const underDirs = [];
@@ -192,37 +154,17 @@ export const Index: NextPage = (props) => {
       .then((res) => {
         res.prefixes.forEach((folderRef) => {
           underDirs.push(`${clickedDir}/${folderRef.name}`);
-          // console.log('handleDirSearch folderRef.name:', folderRef.name);
-          // allDirDatas.root[clickedDir][folderRef.name] = { files: '' };
-          // allDirDatas.root[clickedDir][
-          //   folderRef.name
-          // ].path = `${clickedDir}/${folderRef.name}`;
         });
         res.items.forEach((itemRef) => {
-          // console.log('handleDirSearch itemRef.name:', itemRef.name);
           underItems.push(`${itemRef.name}`);
         });
-        // console.log(
-        //   'allDirDatas.root[clickedDir]:',
-        //   allDirDatas.root[clickedDir]
-        // );
-        // allDirDatas.root[clickedDir].files = [...underItems];
-        // console.log('underDirs:', underDirs);
         setStorageDir([...underDirs]);
         setStorageDatas([...underItems]);
         setIsLoading(storageDatas.length === 0 && storageDir.length === 0);
-        // console.log('storageDir:', storageDir);
-
-        // res.items.forEach((itemRef) => {
-        //   console.log('handleDirSearch itemRef.name:', itemRef.name);
-        //   underItems.push(itemRef.name);
-        // });
-        // allDirDatas.root[clickedDir] = [...underItems];
-        // console.log('underItems:', underItems);
       })
       .catch((error) => {
-        alert('handleDirBackToTop エラーが発生しました。');
-        console.log('handleDirBackToTop error:', error);
+        alert('handleDirSearch エラーが発生しました。');
+        console.log('handleDirSearch error:', error);
       });
   };
 
@@ -270,82 +212,21 @@ export const Index: NextPage = (props) => {
       </div>
       <Heading2>Storage表示</Heading2>
       <div className="text-left">
-        <Btn link={false} click={handleDirBackToTop}>
-          storageTOPに戻る
-        </Btn>
         <Text class="break-all">
           現在のディレクトリ
           <br />
           {`https://firebasestorage.googleapis.com/v0/b/udemy-todo-f0672.appspot.com/o/${oldDir}`}
         </Text>
-        {/* <FirebaseStorageFiles loading={isLoading}>
-          ああああああああああ
-        </FirebaseStorageFiles> */}
         <Text>配下にあるファイル・ディレクトリ</Text>
-        {/* {isLoading ? null : null} */}
         <FirebaseStorageFiles datas={storageDatas} loading={isLoading} />
         <FirebaseStorageDirectorys
           datas={storageDir}
           loading={isLoading}
           click={handleDirSearch}
         />
-        {/* {storageDatas &&
-          storageDatas.map((data) => {
-            return (
-              <FirebaseStorageFiles
-                datas={storageDatas}
-                loading={isLoading}
-                key={data}>
-                {data}
-              </FirebaseStorageFiles>
-            );
-          })} */}
-        {/* {storageDir &&
-          storageDir.map((data) => {
-            return (
-              <FirebaseStorageDirectorys
-                loading={isLoading}
-                key={data}
-                value={data}
-                click={handleDirSearch}
-              />
-            );
-          })} */}
-        {/* {isLoading
-          ? storageDatas.map((data) => {
-              return (
-                <FirebaseStorageFiles loading={isLoading} key={data}>
-                  {data}
-                </FirebaseStorageFiles>
-
-                // <Text key={data} class="pl-3 mb-2">
-                //   {data}
-                // </Text>
-              );
-            })
-          : null} */}
-        {/* {isLoading ? (
-          storageDir.map((data) => {
-            return (
-              <FirebaseStorageDirectorys
-                loading={isLoading}
-                key={data}
-                value={data}
-                click={handleDirSearch}></FirebaseStorageDirectorys>
-              // <div key={data} className="-mt-2">
-              //   <input
-              //     className="text-sm cursor-pointer sm:hover:opacity-80 text-blue-400 pl-3 inline-block mb-2"
-              //     type="button"
-              //     value={data}
-              //     onClick={handleDirSearch}
-              //   />
-              //   <span className="text-xs">ディレクトリ</span>
-              // </div>
-            );
-          })
-        ) : (
-          <LoadingText />
-        )} */}
+        <Btn link={false} click={handleDirBackToTop}>
+          storageTOPに戻る
+        </Btn>
       </div>
     </>
   );
