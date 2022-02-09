8eea3c369705bb4329304d29f6fbad471b44b3d8
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-09 11:48:21 +0900

    ♻️ カラムコンポーネントのクラスをpropsで渡すとうまく対応できないので、classを追加する方法に改修

--- src/components/Column/ColumnItem.tsx
+++ src/components/Column/ColumnItem.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 
 type PROPS = {
-  col: string;
-  colSm: string;
   class?: string;
   children: React.ReactNode;
 };
 
 export const ColumnItem: React.VFC<PROPS> = (props) => {
-  // console.log('props.col:', `md:w-${props.col}/12`);
   const addColumnClass = props.class ? ` ${props.class}` : '';
 
   return (
-    <div
-      className={`w-full sm:w-${props.colSm}/6 md:w-${props.col}/12 px-2 pt-2${addColumnClass}`}>
-      {props.children}
-    </div>
+    <div className={`w-full px-2 pt-2${addColumnClass}`}>{props.children}</div>
   );
 };
