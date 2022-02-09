2163798f4de72f4bc35eeed6972441c86198a9bf
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-09 11:49:25 +0900

    ♻️ ログイン時のローディング画面のSP時、TAB時調整

--- src/components/Loading/LoadingTop.tsx
+++ src/components/Loading/LoadingTop.tsx
@@ -5,7 +5,7 @@ export const LoadingTop: React.VFC = () => {
     <div className="border border-gray-100 shadow rounded-md p-4 w-full">
       <div className="animate-pulse">
         <div className="h-10 w-5/12 mb-8 bg-gray-400 rounded"></div>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div className="h-8 bg-gray-400 rounded"></div>
           <div className="h-8 bg-gray-400 rounded"></div>
           <div className="h-8 bg-gray-400 rounded"></div>
