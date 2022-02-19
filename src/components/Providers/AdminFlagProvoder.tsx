85fd6276b12b23796d1c7abee927d39500e31ff4
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-19 12:33:43 +0900

    🐛 分割代入でエラーになっていたのをどうにか修正

--- src/components/Providers/AdminFlagProvoder.tsx
+++ src/components/Providers/AdminFlagProvoder.tsx
@@ -1,21 +1,29 @@
 import React, { useState, createContext } from 'react';
 
+interface CONTEXTPROPS {
+  isAdmin?: Boolean;
+  SetIsAdmin?: any;
+}
+
 type PROPS = {
   isAdmin?: Boolean;
+  SetIsAdmin?: any;
   children?: React.ReactNode;
 };
 
-export const AdminFlagContext = createContext({});
+export const AdminFlagContext = createContext<CONTEXTPROPS>({});
 
 export const AdminFlagProvoder = (props: PROPS) => {
+  const { children } = props;
   const [isAdmin, SetIsAdmin] = useState(false);
+  console.log('isAdmin:', isAdmin);
 
   // const sampleObj = {
   //   sampleValue: 'sampleValue',
   // };
   return (
     <AdminFlagContext.Provider value={{ isAdmin, SetIsAdmin }}>
-      {props.children}
+      {children}
     </AdminFlagContext.Provider>
   );
 };
