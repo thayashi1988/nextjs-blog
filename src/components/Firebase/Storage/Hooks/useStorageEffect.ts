407df3fecc7190d9905b6a276663f9cbed652fcd
tatsuro.hayashi <tatsurouhayashi0210@gmail.com>
2022-02-21 22:41:46 +0900

    ♻️ useEffectのクリーンアップ関数を入れてみる

--- src/components/Firebase/Storage/Hooks/useStorageEffect.ts
+++ src/components/Firebase/Storage/Hooks/useStorageEffect.ts
@@ -1,32 +1,33 @@
 import { useEffect } from 'react';
-import type { USEEFFECTSTATES } from '@/components/Firebase/Storage/Hooks/types';
+import type { STATESPROPS } from '@/components/Firebase/Storage/Hooks/types';
 import { storageRef } from '../../../../../firebase';
 
-export const useStorageEffect = (props: USEEFFECTSTATES) => {
+export const useStorageEffect = (props: STATESPROPS) => {
   const { setStorageDirs, setStorageDatas, setIsLoading } = props;
   useEffect(() => {
-    const underItems = [];
-    const underDirs = [];
-    let unmounted = false;
-    storageRef
-      .listAll()
-      .then((res) => {
-        res.prefixes.forEach((folderRef) => {
-          underDirs.push(folderRef.name);
-          res.items.forEach((itemRef) => {
-            underItems.push(itemRef.name);
+    // let unmounted = false;
+    const firstLoad = function () {
+      const underItems = [];
+      const underDirs = [];
+      storageRef
+        .listAll()
+        .then((res) => {
+          res.prefixes.forEach((folderRef) => {
+            underDirs.push(folderRef.name);
+            res.items.forEach((itemRef) => {
+              underItems.push(itemRef.name);
+            });
           });
+          setStorageDirs([...underDirs]);
+          setStorageDatas([...underItems]);
+          setIsLoading(underItems.length === 0 && underDirs.length === 0);
+        })
+        .catch((error) => {
+          alert('useEffect エラーが発生しました。');
+          console.log('useEffect error:', error);
         });
-        setStorageDirs([...underDirs]);
-        setStorageDatas([...underItems]);
-        setIsLoading(underItems.length === 0 && underDirs.length === 0);
-      })
-      .catch((error) => {
-        alert('useEffect エラーが発生しました。');
-        console.log('useEffect error:', error);
-      });
-    return () => {
-      unmounted = true;
     };
+    firstLoad();
+    return () => firstLoad();
   }, []);
 };
